b358fe75064aa7e30af92965a997cdafb1426ecd
CarolinaCarol <100369787+CarolinaCarol@users.noreply.github.com>
2022-03-24 13:01:21 -0300

    Create codigo.ino

---
+++ exercicios/ex1/codigo.ino
@@ -0,0 +1,17 @@
+codigo:
+
+const in led = 2;
+
+void setup()  
+)
+  pinMODE(led, OUTPUT);
+)
+  
+void loop()
+{
+  DIGITALwRITE(LED, high);
+  DELAY(500);
+  digitalWrite(led, LOW);
+  delay(500);
+)
+ 
